ae957caf05fa54967bac9b850040cd4e49dbb72e
Bryant Vail <Bryanttv@outlook.com>
2018-06-08 16:53:18 -0400

    done

--- main.js
+++ main.js
@@ -11,8 +11,89 @@
     4   -   completed
 */
 
+// function callback(val){
+//     console.log(
+//         val
+//     );
+// }
+
 window.onload = function(){
 
+    // let fruits = ["banana", "apple", "orange"];
+    // // fruits.forEach(function(val){
+    // //     console.log(val);
+    // // });
+
+    // fruits.forEach(callback);
+
+    // function cb(data){
+    //     console.log(
+    //         data
+    //     );
+    // }
+
+
+    // $.get("data/tweets.json"/*startNow*/, function(data){
+    //     console.log(
+    //         data
+    //     );
+    // });
+
+    // $.get("data/tweets.json",cb);
+
+    // console.log(
+    //     "test"
+    // );
+
+
+    //error handler
+    function handleError(jqXHR, textStatus, error){
+        console.log(
+            error
+        );
+    }//end handleError
+
+    function callbackLog(data){
+        console.log(
+            data
+        )
+    }
+
+    $.ajax({
+        type:   "GET",
+        url:    "data/tweets.json",
+        success:function(data){
+            console.log(
+                data
+            );
+            $.ajax({
+                type:   "GET",
+                url:    "data/friends.json",
+                success:function(data){
+                    console.log(
+                        data
+                    );
+                    $.ajax({
+                        type:"GET",
+                        url:    "data/videos.json",
+                        success:function(data){
+                            console.log(
+                                data
+
+                            );
+                        },
+                        error:handleError
+                    });//end $.ajax
+                },
+                error:handleError
+            })//end $.ajax
+        },
+        error:handleError
+    });//end $.ajax
+
+    console.log(
+        "test"
+    );
 
 };
 
