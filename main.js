41770399ecd7221a04030eda70c857a44a5c576d
Bryant Vail <Bryanttv@outlook.com>
2018-06-08 17:34:25 -0400

    done

--- main.js
+++ main.js
@@ -15,6 +15,49 @@
 
 window.onload = function(){
 
+    $.get("data/tweets.json")
+        .then(function(tweets){
+            console.log(tweets);
+            return $.get("data/friends.json");
+        }).then(function(friends){
+            console.log(friends);
+            return $.get("data/videos.json");
+        }).then((videos)=>{
+            console.log(videos);
+        });
+
+    // function get(url){
+    //     return new Promise(function(resolve, reject){
+    //         let xhttp = new XMLHttpRequest();
+    //         xhttp.open("GET", url, true);
+    //         xhttp.onload = function(){
+    //             if(xhttp.status == 200){
+    //                 resolve(JSON.parse(xhttp.response));
+    //             }else{
+    //                 reject(xhttp.statusText);
+    //             }
+    //         };
+    //         xhttp.onerror = function(){
+    //             reject(xhttp.statusText);
+    //         };
+    //         xhttp.send();
+    //     });
+    // }//end function get(url){
+
+    // let promise = get("data/tweets.json");
+    // promise.then(function(tweets){
+    //     console.log(tweets);
+    //     return get("data/friends.json");
+    // }).then(function(friends){
+    //     console.log(friends);
+    //     return get("data/videos.json");
+    // }).then((videos) => {
+    //     console.log(videos);
+    // }).catch(function(error){
+    //     console.log(
+    //         error
+    //     );
+    // });
 
 };
 
