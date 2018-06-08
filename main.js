ef74e3f5ecd6f2e125b7f10a59fab49e62d71299
Bryant Vail <Bryanttv@outlook.com>
2018-06-08 18:45:13 -0400

    complete

--- main.js
+++ main.js
@@ -16,6 +16,45 @@
 window.onload = function(){
 
 
+    genWrap(function*(){
+        let tweets =    yield $.get("data/tweets.json");
+        console.log(tweets);
+        let friends =   yield $.get("data/friends.json");
+        console.log(friends);
+        let videos =    yield $.get("data/videos.json");
+        console.log(videos);
+
+    });
+
+    // libraries: 'q' or 'bluebird', recommended by this host
+    function genWrap(generator){
+        let gen = generator();
+
+        function handle(yielded){
+            if(!yielded.done){
+                yielded.value.then(function(data){
+                    return handle(gen.next(data));
+                });
+            }
+        }//end handle
+
+        return handle(gen.next());
+    }//end genWrap
+
+    
+
+    // // // // // // //
+
+    // function* gen(){
+    //     //pausable functions
+    //     let x = yield 10;
+    // }
+
+    // let myGen = gen();
+    // console.log(
+    //     myGen.next()
+    // );
+    // console.log(myGen.next(10));
 
 };
 
